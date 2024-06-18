0308688faef925ceb10f83d025e88696df475b49
godhaniaesha <aesha.kalathiyainfotech@gmail.com>
2024-06-18 14:55:35 +0530

    git branch intro

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Today
+          Learn React Today (Git Branch Intro)
         </a>
       </header>
     </div>
